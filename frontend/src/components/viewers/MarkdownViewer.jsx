83ee4b90b12b2d5fca3b24ec6d729227dbe33f56
Chris Vilches <felovilches@gmail.com>
2026-03-13 22:49:15 +0900

    add code markdown component

--- frontend/src/components/viewers/MarkdownViewer.jsx
+++ frontend/src/components/viewers/MarkdownViewer.jsx
@@ -104,6 +104,17 @@ export default function MarkdownViewer({ file, content, mtime }) {
               />
             );
           },
+          code({ children, inline, node, ...props }) {
+            const { start, end } = node.position
+            if (start.line === end.line) {
+              return (
+                <code className="bg-[#2d2d2d] before:content-none after:content-none text-[#ffb454] font-mono text-[0.9em] px-1.5 py-0.5 rounded">
+                  {children}
+                </code>
+              )
+            }
+            return <code>{children}</code>
+          }
         }}
       >
         {content || ""}
