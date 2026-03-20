ef6a19e1edb4d767d8537bfa612cd71f40ab5531
Chris Vilches <felovilches@gmail.com>
2026-03-20 22:41:55 +0900

    Warning: removed overflow-x-hidden from <main> (BETA, needs testing), create table component (scrollable), refactor ulComponent (into function), apply scrollbar styling globally, add TODO

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -129,7 +129,7 @@ export default function Layout() {
           )}
         </div>
 
-        <main className="flex-1 overflow-y-auto overflow-x-hidden bg-gray-950 scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-transparent hover:scrollbar-thumb-gray-600">
+        <main className="flex-1 overflow-y-auto bg-gray-950 scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-transparent hover:scrollbar-thumb-gray-600">
           <Outlet context={{ setLayoutTopContent }} />
         </main>
       </div>
