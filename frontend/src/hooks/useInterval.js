05fe2060654e34dad866b56a44327558e0458514
Chris Vilches <felovilches@gmail.com>
2026-03-28 22:05:01 +0900

    resolve TODO

--- frontend/src/hooks/useInterval.js
+++ frontend/src/hooks/useInterval.js
@@ -1,6 +1,5 @@
 import { useEffect, useEffectEvent } from "react";
 
-// TODO: re-test this (make sure numbers change, etc)
 export default function useInterval(callback, delay) {
   // TODO: study this hook (what problem it solves, etc)
   const onTick = useEffectEvent(callback);
