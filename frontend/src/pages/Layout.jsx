b52cc3ac4d54f0992af2d455b18a6647b9c3a745
Chris Vilches <felovilches@gmail.com>
2026-05-08 23:53:31 +0900

    Fix this task by removing the useResizable (it messed up the screen width, causing the issue. Apparently the reason was that the user specified size was in pixels, but this caused the final pixel values to get rounded, causing an extra pixel at the rightmost position):
    > I see the scrolling issue again (it selects text). It can be reproduced
    > in edit mode (placing the cursor at the rightmost position and dragging
    > the scroll will instead select text). Outside edit mode, it only happens
    > sometimes, depending on the zoom of the browser (try different values
    > until it triggers). It seems the sidemenu size is also a deciding factor
    > of whether it happens or not, so move that too.

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -13,7 +13,6 @@ import SearchBar from "../components/SearchBar";
 import SettingsModal from "../components/SettingsModal";
 import Sidemenu from "../components/Sidemenu";
 import useKeyShortcut from "../hooks/useKeyShortcut";
-import useResizable from "../hooks/useResizable";
 
 function useBookmarksModal() {
   const [isOpen, setIsOpen] = useState(false);
@@ -39,16 +38,6 @@ export default function Layout() {
   const [searchModalOpen, setSearchModalOpen] = useState(false);
   const [settingsModalOpen, setSettingsModalOpen] = useState(false);
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const {
-    ref: sidebarRef,
-    size: sidebarWidth,
-    onHandleMouseDown,
-  } = useResizable({
-    defaultValue: 288,
-    min: 180,
-    max: 600,
-    storageKey: "sidebarWidth",
-  });
   const [layoutTopContent, setLayoutTopContent] = useState({
     title: "",
     extra: null,
@@ -79,11 +68,11 @@ export default function Layout() {
   };
 
   return (
-    <div className="flex h-screen bg-gray-950">
+    <div className="h-screen flex overflow-hidden bg-gray-950">
       <Dialog open={sidebarOpen} onClose={setSidebarOpen} className="relative z-50 md:hidden">
         <DialogBackdrop className="fixed inset-0 bg-black/60" />
         <div className="fixed inset-0 flex">
-          <DialogPanel className="w-72 h-full bg-gray-900 border-r border-gray-800 flex flex-col">
+          <DialogPanel className="w-5/6 h-full bg-gray-900 border-r border-gray-800 flex flex-col">
             <Sidemenu
               {...sideMenuProps}
               onClose={() => setSidebarOpen(false)}
@@ -92,20 +81,11 @@ export default function Layout() {
         </div>
       </Dialog>
 
-      <aside
-        ref={sidebarRef}
-        className="hidden md:flex flex-shrink-0 bg-gray-900 border-r border-gray-800 flex-col relative"
-        style={{ width: sidebarWidth }}
-      >
-        {/* biome-ignore lint/a11y/noStaticElementInteractions: drag resize handle */}
-        <div
-          onMouseDown={onHandleMouseDown}
-          className="absolute right-[-4px] top-0 bottom-0 w-1 cursor-col-resize hover:bg-indigo-500/50 active:bg-indigo-500/70 z-50 shrink-0"
-        />
+      <aside className="hidden md:flex flex-shrink-0 w-64 lg:w-72 xl:w-80 bg-gray-900 border-r border-gray-800 flex-col">
         <Sidemenu {...sideMenuProps} />
       </aside>
 
-      <div className="flex flex-col flex-1 min-w-0">
+      <main className="flex-1 flex flex-col overflow-y-auto bg-gray-950">
         <div className="sticky top-0 z-10 flex items-center gap-3 px-4 h-14 border-b border-gray-800 bg-gray-900 shrink-0">
           <button
             type="button"
@@ -125,10 +105,10 @@ export default function Layout() {
           )}
         </div>
 
-        <main className="flex-1 overflow-y-auto bg-gray-950">
+        <div className="flex-1 min-h-0">
           <Outlet context={{ setLayoutTopContent }} />
-        </main>
-      </div>
+        </div>
+      </main>
 
       <Modal
         open={bookmarksModal.isOpen}
